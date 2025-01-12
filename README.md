cc3aaab6f90d013cb5ea009a4f46bc620600be2a
FHobbs8030 <FHobbs8030@Gmail.com>
2025-01-12 05:05:18 -0800

    Intial commit for Trees of Northern Nevada

---
+++ README.md
@@ -0,0 +1,38 @@
+# se_project_aroundtheus
+
+This is the final project Sprint 5 of the Software Engineering program at TripleTen. It was created using Modals and java script introductory, based on the design brief.
+
+## Project features
+
+- modals
+- Semantic HTML5
+- Flexbox
+- Positioning
+- Flat BEM file structure
+- Responsive Design
+
+## The Plans on improving this project are
+
+-This page is presently under construction for Upgrades using javaScript
+
+=========================================
+
+## Take a Look at the Project
+
+click [here](https://FHobbs8030.github.io/se_project_aroundtheus/)
+
+=========================================
+
+## Watch the Presentation
+
+click [here](https://github.com/user-attachments/assets/5cfacafb-053e-448f-a66e-2a506c7a0679)
+
+========================================================
+
+![1280px](https://github.com/user-attachments/assets/0ba21fc6-f4e6-43d0-9dd4-2e2cd889f56f)
+
+![800px](https://github.com/user-attachments/assets/36696942-99dc-4fc1-96cb-fc53c40de836)
+
+![320px](https://github.com/FHobbs8030/se_project_aroundtheus/assets/63701522/8fb14860-bfbf-4ae8-92b8-7eedb6f5e40a)
+
+==============================================
---
+++ scripts/index.js
@@ -0,0 +1,201 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const imageModal = document.querySelector("#image-modal");
+  const modalImage = imageModal.querySelector(".modal__image");
+  const modalCaption = imageModal.querySelector(".modal__caption");
+
+  const template = document.querySelector("#card-template");
+
+  const openAddModalButton = document.querySelector(".profile__add-button");
+  const openEditModalButton = document.querySelector(".profile__edit-button");
+  const addModal = document.getElementById("add-modal");
+  const editModal = document.getElementById("edit-modal");
+  const editForm = document.forms["edit-form"];
+
+  const closeEditModalButton = editModal.querySelector(".modal__close-button");
+  const closeAddModalButton = addModal.querySelector(".modal__close-button");
+  const closeImageModalButton = document.querySelector("#image-modal-close");
+
+  const cardForm = document.forms["card-form"];
+
+  const nameInput = document.querySelector("#name");
+  const aboutMeInput = document.querySelector("#about_me");
+  const placeInput = document.querySelector("#place");
+  const linkInput = document.querySelector("#link");
+
+  const profileName = document.querySelector(".profile__name");
+  const profileAboutMe = document.querySelector(".profile__about_me");
+  const cardsContainer = document.querySelector("#cards-container");
+
+  const imageLibrary = {
+    "The Black Cottonwood": {
+      name: "The Black Cottonwood",
+      link: "http://127.0.0.1:5500/images/card-images/black_cottonwood_summer.webp",
+      alt: "A beautiful landscape of the Black Cottonwood",
+    },
+    "Yosemite Valley": {
+      name: "Yosemite Valley",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+      alt: "Yosemite Valley",
+    },
+    "Lake Louise": {
+      name: "Lake Louise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+      alt: "Lake Louise",
+    },
+    "Bald Mountains": {
+      name: "Bald Mountains",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+      alt: "Bald Mountains",
+    },
+    Latemar: {
+      name: "Latemar",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+      alt: "Latemar",
+    },
+    "Vanoise National Park": {
+      name: "Vanoise National Park",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+      alt: "Vanoise National Park",
+    },
+    "Lago di Braies": {
+      name: "Lago di Braies",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+      alt: "Lago di Braies",
+    },
+  };
+
+  const initialCards = [
+    imageLibrary["Yosemite Valley"],
+    imageLibrary["Lake Louise"],
+    imageLibrary["Bald Mountains"],
+    imageLibrary["Latemar"],
+    imageLibrary["Vanoise National Park"],
+    imageLibrary["Lago di Braies"],
+  ];
+
+  function getCardElement(data) {
+    const cardElement = template.content.cloneNode(true).firstElementChild;
+    const image = cardElement.querySelector(".cards__image");
+    const title = cardElement.querySelector(".cards__title");
+    const deleteButton = cardElement.querySelector(".cards__delete-button");
+    const heartButton = cardElement.querySelector(".cards__heart");
+
+    image.src = data.link;
+    image.alt = data.alt;
+    title.textContent = data.name;
+
+    deleteButton.addEventListener("click", (event) => {
+      event.stopPropagation();
+      cardElement.remove();
+    });
+
+    heartButton.addEventListener("click", (event) => {
+      event.stopPropagation();
+      heartButton.classList.toggle("cards__heart__active");
+    });
+
+    image.addEventListener("click", () => {
+      modalImage.src = data.link;
+      modalImage.alt = data.name;
+      modalCaption.textContent = data.name;
+      openPopup(imageModal);
+    });
+
+    return cardElement;
+  }
+
+  initialCards.forEach((cardData) => {
+    const cardElement = getCardElement(cardData);
+    cardsContainer.appendChild(cardElement);
+  });
+
+  placeInput.addEventListener("input", () => {
+    const placeName = placeInput.value.trim();
+    if (imageLibrary[placeName]) {
+      linkInput.value = imageLibrary[placeName].link;
+    } else {
+      linkInput.value = "";
+    }
+  });
+
+  function openPopup(modal) {
+    modal.classList.remove("modal_hidden");
+    modal.classList.add("modal_open");
+  }
+
+  function closePopup(modal) {
+    modal.classList.remove("modal_open");
+    modal.classList.add("modal_hidden");
+  }
+
+  closeImageModalButton.addEventListener("click", () => {
+    closePopup(imageModal);
+  });
+
+  openEditModalButton.addEventListener("click", () => {
+    nameInput.value = profileName.textContent;
+    aboutMeInput.value = profileAboutMe.textContent;
+    openPopup(editModal);
+  });
+
+  closeEditModalButton.addEventListener("click", () => {
+    closePopup(editModal);
+  });
+
+  openAddModalButton.addEventListener("click", () => {
+    openPopup(addModal);
+  });
+
+  closeAddModalButton.addEventListener("click", () => {
+    closePopup(addModal);
+  });
+
+  editForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    profileName.textContent = nameInput.value;
+    profileAboutMe.textContent = aboutMeInput.value;
+
+    closePopup(editModal);
+  });
+
+  cardForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const placeName = placeInput.value.trim();
+    const link = linkInput.value.trim();
+
+    if (!isValidUrl(link)) {
+      alert("Please enter a valid URL.");
+      return;
+    }
+
+    let newPlace;
+    if (imageLibrary[placeName]) {
+      newPlace = imageLibrary[placeName];
+    } else {
+      newPlace = {
+        name: placeName,
+        link: link,
+        alt: placeName,
+      };
+    }
+
+    const newCardElement = getCardElement(newPlace);
+    cardsContainer.prepend(newCardElement);
+
+    placeInput.value = "";
+    linkInput.value = "";
+
+    closePopup(addModal);
+  });
+
+  function isValidUrl(string) {
+    try {
+      new URL(string);
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
+});
